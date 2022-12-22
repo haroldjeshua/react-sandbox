4648269af690ad0137c76263f838786cab45b76b
Harold Varde <harvardii15@gmail.com>
2022-12-23 02:15:38 +0800

    [proj2] MOD: fixing bug for rendering slider wrapper items

--- slider-hover/src/App.jsx
+++ slider-hover/src/App.jsx
@@ -1,164 +1,117 @@
 import { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
 import "./App.scss";
 
-const slideData = [
-  {
-    index: 0,
-    headline: "New Fashion Apparel",
-    button: "Shop now",
-    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg",
-  },
-  {
-    index: 1,
-    headline: "In The Wilderness",
-    button: "Book travel",
-    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg",
-  },
-  {
-    index: 2,
-    headline: "For Your Current Mood",
-    button: "Listen",
-    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg",
-  },
-  {
-    index: 3,
-    headline: "Focus On The Writing",
-    button: "Get Focused",
-    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg",
-  },
-];
+function App() {
+  const slideData = [
+    {
+      index: 0,
+      headline: "New Fashion Apparel",
+      button: "Shop now",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg",
+    },
+    {
+      index: 1,
+      headline: "In The Wilderness",
+      button: "Book travel",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg",
+    },
+    {
+      index: 2,
+      headline: "For Your Current Mood",
+      button: "Listen",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg",
+    },
+    {
+      index: 3,
+      headline: "Focus On The Writing",
+      button: "Get Focused",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg",
+    },
+  ];
 
-const Slide = ({ slide, current, handleSlideClick }) => {
+  const [current, setCurrent] = useState(0);
   const slideRef = useRef(null);
 
-  const handleMouseMove = (e) => {
-    const el = e.current;
+  const handleMouseMove = (event) => {
+    const el = slideRef.current;
     const r = el.getBoundingClientRect();
 
-    el.style.setProperty("--x", e.clientX - r.left + Math.floor(r.width / 2));
-    el.style.setProperty("--y", e.clientY - r.top + Math.floor(r.height / 2));
+    el.style.setProperty(
+      "--x",
+      event.clientX - (r.left + Math.floor(r.width / 2))
+    );
+    el.style.setProperty(
+      "--y",
+      event.clientY - (r.top + Math.floor(r.height / 2))
+    );
   };
 
-  const handleMouseLeave = (e) => {
-    e.current.style.setProperty("--x", 0);
-    e.current.style.setProperty("--y", 0);
-  };
-
-  const imageLoaded = (e) => {
-    e.target.style.opacity = 1;
-  };
-
-  useEffect(() => {
-    slideRef.current.addEventListener("mousemove", handleMouseMove);
-    slideRef.current.addEventListener("mouseleave", handleMouseLeave);
-    slideRef.current.addEventListener("click", handleSlideClick);
-
-    return () => {
-      slide.current.removeEventListener("mousemove", handleMouseMove);
-      slide.current.removeEventListener("mouseleave", handleMouseLeave);
-      slide.current.removeEventListener("click", handleSlideClick);
-    };
-  }, [handleSlideClick, slide.index]);
-
-  const { src, button, headline, index } = slide;
-  let classNames = "slide";
-
-  if (current === index) classNames += " slide--current";
-  else if (current - 1 === index) classNames += " slide--previous";
-  else if (current + 1 === index) classNames += " slide--next";
-
-  return (
-    <li
-      ref={slide}
-      className={classNames}
-      onClick={() => handleSlideClick(index)}
-    >
-      <div className="slide__image-wrapper">
-        <img src={src} alt="headline" className="slide__image" />
-      </div>
-
-      <article className="slide__content">
-        <h2 className="slide__headline">{headline}</h2>
-        <button className="slide__action btn">{button}</button>
-      </article>
-    </li>
-  );
-};
-
-const SliderControl = ({ type, title, handleClick }) => {
-  return (
-    <button className={`btn btn--${type}`} title={title} onClick={handleClick}>
-      <svg className="icon" viewBox="0 0 24 24">
-        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-      </svg>
-    </button>
-  );
-};
-
-const Slider = ({ slides, heading }) => {
-  const [current, setCurrent] = useState(0);
-
-  const handlePrevClick = () => {
-    const prev = current - 1;
-    setCurrent(prev < 0 ? slides.length - 1 : prev);
-  };
-
-  const handleNextClick = () => {
-    const next = current + 1;
-    setCurrent(next === slides.length ? 0 : next);
+  const handleMouseLeave = () => {
+    slideRef.current.style.setProperty("--x", 0);
+    slideRef.current.style.setProperty("--y", 0);
   };
 
   const handleSlideClick = (index) => {
-    if (current !== index) {
-      setCurrent(index);
-    }
+    setCurrent(index);
   };
 
-  const headingId = `slider-heading__${heading
-    .replace(/\s+/g, "-")
-    .toLowerCase()}`;
-  const wrapperTransform = {
-    transform: `translateX(-${current * (100 / slides.length)}%)`,
+  const handlePreviousClick = () => {
+    setCurrent(current - 1);
   };
 
-  return (
-    <div className="slider" aria-labelledby={headingId}>
-      <ul className="slider__wrapper" style={wrapperTransform}>
-        <h3 id={headingId} className="visually-hiden">
-          {heading}
-        </h3>
-        {slides.map((slide) => (
-          <Slide
-            key={slide.index}
-            slide={slide}
-            current={current}
-            handleSlideClick={handleSlideClick}
-          />
-        ))}
-      </ul>
+  const handleNextClick = () => {
+    setCurrent(current + 1);
+  };
 
-      <div className="slider__controls">
-        <SliderControl
-          type="previous"
-          title="Go to previous slide"
-          handleClick={handlePrevClick}
-        />
+  const imageLoaded = (event) => {
+    event.target.style.opacity = 1;
+  };
 
-        <SliderControl
-          type="next"
-          title="Go to next slide"
-          handleClick={handleNextClick}
-        />
-      </div>
-    </div>
-  );
-};
+  const Slide = ({ slide }) => {
+    const { src, button, headline, index } = slide;
+    let classNames = "slide";
 
-function App() {
+    if (current === index) classNames += " slide--current";
+    else if (current - 1 === index) classNames += " slide--previous";
+    else if (current + 1 === index) classNames += " slide--next";
+  };
+
+  const SliderControl = ({ type, title, handleClick }) => {
+    return (
+      <button
+        className={`btn btn--${type}`}
+        title={title}
+        onClick={handleClick}
+      >
+        <svg className="icon" viewBox="0 0 24 24">
+          <title>{title}</title>
+          <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+        </svg>
+      </button>
+    );
+  };
+
   return (
     <div className="App">
-      <Slider heading="Test Slider" slides={slideData} />
+      <div className="slider">
+        <ul className="slider__wrapper">
+          {slideData.map((slide) => (
+            <Slide key={slide.index} slide={slide} current={current} />
+          ))}
+        </ul>
+        <div className="slider__controls">
+          <SliderControl
+            type="previous"
+            title="Go to previous slide"
+            handleClick={handlePreviousClick}
+          />
+          <SliderControl
+            type="next"
+            title="Go to next slide"
+            handleClick={handleNextClick}
+          />
+        </div>
+      </div>
     </div>
   );
 }
