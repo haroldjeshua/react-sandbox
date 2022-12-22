43f41e79102a0a4eb5e1e840bbcc30df76dfc28e
Harold Varde <harvardii15@gmail.com>
2022-12-23 00:25:51 +0800

    [proj2] MOD: add SliderControl comp

--- slider-hover/src/App.jsx
+++ slider-hover/src/App.jsx
@@ -45,10 +45,6 @@ const Slide = ({ slide, current, handleSlideClick }) => {
     e.current.style.setProperty("--y", 0);
   };
 
-  const handleSlideClick = (e) => {
-    handleSlideClick(slide.index);
-  };
-
   const imageLoaded = (e) => {
     e.target.style.opacity = 1;
   };
@@ -90,7 +86,21 @@ const Slide = ({ slide, current, handleSlideClick }) => {
   );
 };
 
+const SliderControl = ({ type, title, handleClick }) => {
+  return (
+    <button className={`btn btn--${type}`} title={title} onClick={handleClick}>
+      <svg className="icon" viewBox="0 0 24 24">
+        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+      </svg>
+    </button>
+  );
+};
+
 function App() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleSlideClick = (index) => setCurrentSlide(index);
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
