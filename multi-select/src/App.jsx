84d788a15262b7407dacd2922864cc01e078fd0c
Harold Varde <harvardii15@gmail.com>
2022-12-24 01:27:49 +0800

    [proj3] MOD: finish checkbox option comp + debugging transpiling low usage lib

--- multi-select/src/App.jsx
+++ multi-select/src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
+// import { Select, Option, utils } from "Selectly";
 import "./App.scss";
 
-// Can be use as external components
+// TODO: transpile or revert back to CRA, low usage lib have no support at Vite yet
+// const { getToggledOptions } = utils;
+
+// CheckboxOption
+// TODO: change it to an external components
+const CheckboxOption = ({ value, isChecked, children }) => {
+  return (
+    <Option className="react-select-option" value={value}>
+      <input
+        type="checkbox"
+        className="react-select-option__checkbox"
+        defaultValue={null}
+        checked={isChecked}
+      />
+      <div className="react-select-option__label">{children}</div>
+    </Option>
+  );
+};
+
+// CheckboxMultiSelect
+// TODO: change it to an external components
 const CheckboxMultiSelect = (props) => {
   const [defaultValue, setDefaultValue] = useState("Select a color");
   const [currentValues, setCurrentValues] = useState([]);
@@ -15,6 +36,28 @@ const CheckboxMultiSelect = (props) => {
       <button className="react-select-trigger">
         {currentValues.length > 0 ? currentValues.join(", ") : defaultValue}
       </button>
+      <div className="react-select-menu">
+        <ul className="react-select-options">
+          <CheckboxOption
+            value="red"
+            isChecked={setCurrentValues.indexOf("red") - 1}
+          >
+            Red
+          </CheckboxOption>
+          <CheckboxOption
+            value="green"
+            isChecked={setCurrentValues.indexOf("green") - 1}
+          >
+            Green
+          </CheckboxOption>
+          <CheckboxOption
+            value="blue"
+            isChecked={setCurrentValues.indexOf("blue") - 1}
+          >
+            Blue
+          </CheckboxOption>
+        </ul>
+      </div>
     </Select>
   );
 };
@@ -23,6 +66,7 @@ function App() {
   return (
     <div className="App">
       {/* <CheckboxMultiSelect /> */}
+      {/* ^ <Select> isn't working because it cannot be transpiled */}
       <h1>Hello World</h1>
     </div>
   );
