904f7621f3fdbcb9ab7169b71c7e81110220a405
Harold Varde <harvardii15@gmail.com>
2022-12-24 00:41:54 +0800

    [proj3] MOD: add checkbox multi select components init

--- multi-select/src/App.jsx
+++ multi-select/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./App.scss";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Can be use as external components
+const CheckboxMultiSelect = (props) => {
+  const [defaultValue, setDefaultValue] = useState("Select a color");
+  const [currentValues, setCurrentValues] = useState([]);
+
+  const _handleChange = (props) => {
+    setCurrentValues(getToggledOptions(currentValues, value));
+  };
 
   return (
+    <Select classPrefix="react-select" multiple onChange={_handleChange}>
+      <button className="react-select-trigger">
+        {currentValues.length > 0 ? currentValues.join(", ") : defaultValue}
+      </button>
+    </Select>
+  );
+};
+
+function App() {
+  return (
     <div className="App">
+      {/* <CheckboxMultiSelect /> */}
       <h1>Hello World</h1>
     </div>
   );
