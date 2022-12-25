79f96640e01da8adffc29baf632741c2def8fab9
Harold Varde <harvardii15@gmail.com>
2022-12-25 23:56:23 +0800

    ADD: new test

--- classic-tictactoe/src/App.jsx
+++ classic-tictactoe/src/App.jsx
@@ -14,7 +14,10 @@ function App() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
-    const nextSquares = squares.slice();
+    if (squares[i]) {
+      return;
+    }
+    let nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -43,6 +46,27 @@ function App() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
