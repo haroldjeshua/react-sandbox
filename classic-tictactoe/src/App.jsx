2c2d625a8e2b31a1179a58cdad2d2cee16e07e98
Harold Varde <harvardii15@gmail.com>
2022-12-25 23:45:24 +0800

    ADD: new test

--- classic-tictactoe/src/App.jsx
+++ classic-tictactoe/src/App.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -18,22 +12,28 @@ function Square() {
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square values={squares[0]} />
-        <Square values={squares[1]} />
-        <Square values={squares[2]} />
+        <Square values={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square values={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square values={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square values={squares[3]} />
-        <Square values={squares[4]} />
-        <Square values={squares[5]} />
+        <Square values={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square values={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square values={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square values={squares[6]} />
-        <Square values={squares[7]} />
-        <Square values={squares[8]} />
+        <Square values={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square values={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square values={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
