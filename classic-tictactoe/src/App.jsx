0d1d8c1a52d780274254fd6bf41c5155c2d324e7
Harold Varde <harvardii15@gmail.com>
2022-12-25 23:17:10 +0800

    ADD: new test

---
+++ classic-tictactoe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+function App() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+
+export default App;
