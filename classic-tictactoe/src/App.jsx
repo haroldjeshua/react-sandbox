7ef4afd645b3c1da6c2d7ea813d58090f0f9ad2c
Harold Varde <harvardii15@gmail.com>
2022-12-26 14:46:43 +0800

    MOD: adding history + styles

--- classic-tictactoe/src/App.jsx
+++ classic-tictactoe/src/App.jsx
@@ -63,13 +63,30 @@ function App() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {}
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
