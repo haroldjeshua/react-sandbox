02fd0d94cfbaa6d7f7ef7c92205ea0d033e5109b
Harold Varde <harvardii15@gmail.com>
2022-12-29 00:45:19 +0800

    MOD: add logic for arrows onclick

--- image-slider/src/App.jsx
+++ image-slider/src/App.jsx
@@ -21,19 +21,35 @@ function App() {
     },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <div className="max-w-[1400px] h-[700px] w-full m-auto py-16 px-4 relative group">
       <div
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-        style={{ backgroundImage: `url(${slides[3].url})` }}
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
       />
 
-      {/* Arrows */}
+      {/* Left Arrows */}
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black-200 text-white cursor-pointer">
-        <FiChevronLeft size={32} />
+        <FiChevronLeft size={32} onClick={prevSlide} />
       </div>
+
+      {/* Right Arrows */}
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black-200 text-white cursor-pointer">
-        <FiChevronRight size={32} />
+        <FiChevronRight size={32} onClick={nextSlide} />
       </div>
     </div>
   );
