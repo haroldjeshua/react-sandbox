bb5f93ff303cf18df9eac05425024fa3c44f1870
Harold Varde <harvardii15@gmail.com>
2023-01-05 14:03:24 +0800

    ADD: markup + navbar comp

--- chat-app-react/src/App.js
+++ chat-app-react/src/App.js
@@ -1,7 +1,16 @@
+import Navbar from "./components/Navbar";
+
+const style = {
+  appWrapper: `max-w-[728px] mx-auto text-center`,
+  section: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className={style.appWrapper}>
+      <section className={style.section}>
+        <Navbar />
+      </section>
     </div>
   );
 }
