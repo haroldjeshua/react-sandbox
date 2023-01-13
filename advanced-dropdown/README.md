78f40a97f75ba97d4d32419597124698fcda4347
Harold Varde <harvardii15@gmail.com>
2023-01-13 22:24:36 +0800

    MOD: markup + styling + props

---
+++ advanced-dropdown/README.md
@@ -0,0 +1,8 @@
+# Advanced Dropdown Menu
+
+Replicating facebook's dropdown menu.
+
+Notes:
+
+1. Composition: write small reusable components
+2. Props: pass data from parent to child
--- advanced-dropdown/src/App.jsx
+++ advanced-dropdown/src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Navbar>
+      <NavItem icon="69" />
+    </Navbar>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#!" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  );
 }
 
 export default App;
