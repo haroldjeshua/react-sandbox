49016c971d86966468d393c51ca3e0766692532f
Harold Varde <harvardii15@gmail.com>
2023-01-31 23:46:32 +0800

    Automated commit, build and deploy

--- expandable-card/src/App.jsx
+++ expandable-card/src/App.jsx
@@ -7,6 +7,14 @@ function App() {
     <div className="App">
       <motion.div className="card">
         <motion.h2>Framer Motion 🚀</motion.h2>
+        <motion.div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            praesentium quos perferendis nesciunt aspernatur aliquid natus ex
+            magnam!
+          </p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </motion.div>
       </motion.div>
     </div>
   );
