20b54cbad7a1d73bd2dd46dda423594386de69d5
Harold Varde <harvardii15@gmail.com>
2023-01-31 23:53:15 +0800

    Automated commit, build and deploy

--- expandable-card/src/App.jsx
+++ expandable-card/src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, spring } from "framer-motion";
 import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <motion.div className="card">
-        <motion.h2>Framer Motion 🚀</motion.h2>
-        <motion.div>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-            praesentium quos perferendis nesciunt aspernatur aliquid natus ex
-            magnam!
-          </p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        </motion.div>
+      <motion.div
+        className="card"
+        layout
+        transition={{ layout: { duration: 0.75, type: "spring" } }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        {isOpen && (
+          <motion.div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Doloribus praesentium quos perferendis nesciunt aspernatur aliquid
+              natus ex magnam!
+            </p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </motion.div>
+        )}
       </motion.div>
     </div>
   );
