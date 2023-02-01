58ae2d7b6fb8246e7aecb96452702cc2c8ea6fae
Harold Varde <harvardii15@gmail.com>
2023-02-02 00:04:29 +0800

    MOD:  checkout context

--- context-api/src/components/Checkout.jsx
+++ context-api/src/components/Checkout.jsx
@@ -2,12 +2,22 @@ import { useContext } from "react";
 import CartContext from "../CartContext";
 
 const Checkout = () => {
+  const { items } = useContext(CartContext);
+
   return (
     <div className="w-full max-w-xl h-[calc(100vh_-_48px)] mx-auto py-4">
       <div className="flex flex-col gap-4 bg-neutral-800 h-full px-8 py-4 rounded-xl drop-shadow-xl">
-        <h1 className="text-3xl text-center">Checkout</h1>
-        <div className="cart-list">
-          <h2>My Purchases</h2>
+        <h1 className="text-3xl text-center">My Cart</h1>
+        <div className="flex flex-col gap-2">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center gap-4"
+            >
+              <h2 className="text-xl font-bold">{item.name}</h2>
+              <h3 className="text-lg">{item.price}</h3>
+            </div>
+          ))}
         </div>
       </div>
     </div>
