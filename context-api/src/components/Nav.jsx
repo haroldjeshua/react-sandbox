50e2d03891f9bef0fc6d94224280795c857672bd
Harold Varde <harvardii15@gmail.com>
2023-02-01 22:29:22 +0800

    ADD:  components + tw setup

--- context-api/src/components/Nav.jsx
+++ context-api/src/components/Nav.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { FiShoppingBag } from "react-icons/fi";
+import { Link } from "react-routed-dom";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <Link to="/">
+        <h1>Wear</h1>
+      </Link>
+      <Link to="/checkout">
+        <div className="cart">
+          <FiShoppingBag />
+          <span>0</span>
+        </div>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
