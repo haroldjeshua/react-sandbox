0b1bb1d614dbf6b9c66c9a85d7a039422df0e2c4
Harold Varde <harvardii15@gmail.com>
2023-02-01 22:33:37 +0800

    ADD:  product comp

--- context-api/src/components/Products.jsx
+++ context-api/src/components/Products.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState } from "react";
+import Card from "./Card";
 
 const Products = () => {
-  return <div>Products</div>;
+  const products = [
+    { name: "vest", price: 500 },
+    { name: "sweater", price: 750 },
+    { name: "jacket", price: 1000 },
+    { name: "hoodie", price: 800 },
+    { name: "tshirt", price: 650 },
+    { name: "slacks", price: 350 },
+  ];
+
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <Card name={product.name} price={product.price} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
