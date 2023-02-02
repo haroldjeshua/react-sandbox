31b1b14520abfe5bdd0a03ff66f34f162d6c1d0a
Harold Varde <harvardii15@gmail.com>
2023-02-02 12:16:40 +0800

    ADD:  env + fetch

--- anim-filter/src/App.jsx
+++ anim-filter/src/App.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [popular, setPopular] = useState([]);
+
+  console.log(import.meta.env.VITE_REACT_APP_TMDB_API_KEY);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${
+        import.meta.env.VITE_REACT_APP_TMDB_API_KEY
+      }&language=en-US&page=1`
+    );
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.result);
+  };
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
