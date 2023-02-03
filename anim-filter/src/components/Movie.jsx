20cbb21f64394406ed41ab232ba2ed2bb0b27dc0
Harold Varde <harvardii15@gmail.com>
2023-02-03 23:54:50 +0800

    MOD:  movie style

--- anim-filter/src/components/Movie.jsx
+++ anim-filter/src/components/Movie.jsx
@@ -4,8 +4,9 @@ const Movie = ({ movie }) => {
       <img
         src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
         alt={movie.backdrop_path}
+        className="rounded-lg h-64 object-cover"
       />
-      <h2>{movie.title}</h2>
+      <h2 className="text-md leading-4">{movie.title}</h2>
     </div>
   );
 };
