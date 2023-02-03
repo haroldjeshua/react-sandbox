8bd55569029647bebf82e93a88215b458b2a1a55
Harold Varde <harvardii15@gmail.com>
2023-02-04 00:37:20 +0800

    MOD:  filter btn style

--- anim-filter/src/components/Filter.jsx
+++ anim-filter/src/components/Filter.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-
 const Filter = () => {
   return (
     <div className="flex justify-center items-center gap-4 h-24 mb-8">
-      <button className="flex min-w-64">All</button>
-      <button className="flex min-w-64">Action</button>
-      <button className="flex min-w-64">Comedy</button>
+      <button className="flex justify-center items-center gap-2 min-w-24 h-12 font-bold bg-white border border-neutral-500 rounded-lg px-4 transition-all ease-linear hover:bg-violet-300 hover:border-violet-300 active:bg-violet-600 active:border-violet-600">
+        Done All
+      </button>
+      <button className="flex justify-center items-center gap-2 min-w-24 h-12 font-bold bg-white border border-neutral-500 rounded-lg px-4 transition-all ease-linear hover:bg-violet-300 hover:border-violet-300 active:bg-violet-600 active:border-violet-600">
+        Action
+      </button>
+      <button className="flex justify-center items-center gap-2 min-w-24 h-12 font-bold bg-white border border-neutral-500 rounded-lg px-4 transition-all ease-linear hover:bg-violet-300 hover:border-violet-300 active:bg-violet-600 active:border-violet-600">
+        Comedy
+      </button>
     </div>
   );
 };
