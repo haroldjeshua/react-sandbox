fe1dd4d21031945e1a940b8cb7b42171c1cb306a
Harold Varde <harvardii15@gmail.com>
2023-02-06 00:43:55 +0800

    ADD:  dropdown items

--- radix-dropdown/src/App.tsx
+++ radix-dropdown/src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { ReactNode, useState } from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 function App() {
+ let [text, setText] = useState("Select an item")
+
   return (
     <div className="flex min-h-full items-center justify-center">
       <div className="mx-auto w-full max-w-sm overflow-hidden rounded-md border border-gray-300 bg-white">
@@ -12,14 +14,36 @@ function App() {
             </DropdownMenu.Trigger>
 
             <DropdownMenu.Portal>
-              <DropdownMenu.Content align="start" className="mt-1 overflow-hidden rounded bg-white/75 p-2 text-left shadow backdrop-blur">
-
+              <DropdownMenu.Content
+                align="start"
+                className="mt-1 overflow-hidden rounded bg-white/75 p-2 text-left shadow backdrop-blur"
+              >
+                <Item onSelect={() => setText("Clicked Item 1")}>Item 1</Item>
+                <Item onSelect={() => setText("Clicked Item 2")}>Item 2</Item>
+                <Item onSelect={() => alert("😊")}>Item 3</Item>
               </DropdownMenu.Content>
             </DropdownMenu.Portal>
           </DropdownMenu.Root>
         </header>
       </div>
     </div>
+  );
+}
+
+function Item({
+  children,
+  onSelect = () => {},
+}: {
+  children: ReactNode;
+  onSelect?: () => void;
+}) {
+  return (
+    <DropdownMenu.Item
+      onSelect={onSelect}
+      className="w-40 select-none rounded px-2 py-1.5 text-gray-700 data-[highlighted]:bg-sky-400 data-[highlighted]:text-white data-[highlighted]:focus:outline-none"
+    >
+      {children}
+    </DropdownMenu.Item>
   );
 }
 
