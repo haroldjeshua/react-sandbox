8030dd1c546f6ac664eccd211171a1636c75df3f
Harold Varde <harvardii15@gmail.com>
2023-02-08 13:15:25 +0800

    MOD:  steps motion

--- multisteps-fm/src/App.jsx
+++ multisteps-fm/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
   const [step, setStep] = useState(1);
@@ -14,25 +15,29 @@ function App() {
         </div>
         <div className="px-8 pb-8">
           <div>
-            <div className="mt-2 h-6 w-40 rounded bg-slate-100">
-              <div className="mt-4 space-y-2">
-                <div className="h-4 w-5/6 rounded bg-slate-100" />
-                <div className="h-4 rounded bg-slate-100" />
-                <div className="h-4 w-4/6 rounded bg-slate-100" />
-              </div>
+            <div className="mt-2 h-6 w-40 rounded bg-slate-100" />
+            <div className="mt-4 space-y-2">
+              <div className="h-4 w-5/6 rounded bg-slate-100" />
+              <div className="h-4 rounded bg-slate-100" />
+              <div className="h-4 w-4/6 rounded bg-slate-100" />
             </div>
+          </div>
 
-            <div className="flex justify-between mt-10">
-              <button
-                className="rounded px-2 py-1 text-slate-400 hover:text-slate-700"
-                onClick={() => setStep(step > 2 ? step : step - 1)}
-              >
-                Back
-              </button>
-              <button className="flex items-center justify-center rounded-full text-white bg-blue-600 py-1.5 px-1.5 font-medium tracking-tight hover:bg-blue-700 active:bg-blue-800">
-                Continue
-              </button>
-            </div>
+          <div className="flex justify-between mt-10">
+            <button
+              className="rounded px-2 py-1 text-slate-400 hover:text-slate-700"
+              onClick={() => setStep(step < 2 ? step : step - 1)}
+            >
+              Back
+            </button>
+            <button
+              className={`${
+                step > 4 ? "pointer-events-none opacity-50" : ""
+              } flex items-center justify-center rounded-full text-white bg-blue-600 py-1.5 px-3.5 font-medium acking-tight hover:bg-blue-700 active:bg-blue-800`}
+              onClick={() => setStep(step > 4 ? step : step + 1)}
+            >
+              Continue
+            </button>
           </div>
         </div>
       </div>
@@ -49,7 +54,8 @@ function Step({ step, currentStep }) {
       : "complete";
 
   return (
-    <div
+    <motion.div
+      animate={{ backgroundColor: status === "complete" ? "#2563eb" : "#fff" }}
       className={`${
         status === "active"
           ? "border-blue-600 bg-white text-blue-600"
@@ -65,7 +71,7 @@ function Step({ step, currentStep }) {
           <span>{step}</span>
         )}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
