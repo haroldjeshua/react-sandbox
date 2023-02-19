5a2385f607563d05154e53ea34edb8f8db80be92
Harold Varde <harvardii15@gmail.com>
2023-02-19 21:52:47 +0800

    ADD:  markup

--- bubbly-routing/src/App.tsx
+++ bubbly-routing/src/App.tsx
@@ -1,10 +1,36 @@
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { BubblyContainer, BubblyLink } from "react-bubbly-transitions";
 import "./App.css";
+
+const Home = () => <section>Home</section>;
+const Work = () => <section>Work</section>;
+const About = () => <section>About</section>;
+const Contact = () => <section>Contact</section>;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bubbly Transition</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Link to="/">Home</Link>
+              <Link to="/work">Work</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+              <Outlet />
+            </>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="work" element={<Work />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<>No Match</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
