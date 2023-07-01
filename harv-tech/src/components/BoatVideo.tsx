0ac8cf1406e565a1e4fef352c0a600ad9d807852
Harold Varde <harvardii15@gmail.com>
2023-07-02 02:46:27 +0800

    harv-tech: polish anims

--- harv-tech/src/components/BoatVideo.tsx
+++ harv-tech/src/components/BoatVideo.tsx
@@ -1,7 +1,12 @@
 export default function BoatVideo() {
   return (
-    <video className="h-100vh w-full object-cover" autoPlay muted loop>
-        <source src="src/assets/mixkit-sample-vid.mp4" type="video/mp4" />
+    <video
+      className="h-100vh w-full object-cover animate-clip-from-top-animation"
+      autoPlay
+      muted
+      loop
+    >
+      <source src="src/assets/mixkit-sample-vid.mp4" type="video/mp4" />
     </video>
-  )
+  );
 }
--- harv-tech/src/components/MapImages.tsx
+++ harv-tech/src/components/MapImages.tsx
@@ -17,6 +17,18 @@ export default function MapImages() {
         alt="decoration 1"
       />
       <motion.img
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          delay: animationStart + 1.5,
+          type: "tween",
+          duration: 0.5,
+        }}
+        className="object-cover"
+        src="src/assets/decor-3.svg"
+        alt="decoration 3"
+      />
+      <motion.img
         variants={decorRevealLeft}
         initial="hiddenDecor"
         animate="revealedDecor"
