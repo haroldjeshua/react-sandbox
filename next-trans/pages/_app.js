9bcb675869a5f3c06742a776d47f017b86a27477
Harold Varde <harvardii15@gmail.com>
2023-08-11 00:37:27 +0800

    next trans sb:finish

--- next-trans/pages/_app.js
+++ next-trans/pages/_app.js
@@ -1,12 +1,31 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import "./global.css";
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
 
   return (
-    <div key={router.pathname}>
-      <Component {...pageProps} />
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div key={router.pathname}>
+        <Component {...pageProps} />
+
+        <motion.div
+          className="slide-in"
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 1 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+        <motion.div
+          className="slide-out"
+          initial={{ scaleY: 1 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 0 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
